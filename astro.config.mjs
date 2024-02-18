afa103dff4a78a3b1c87f4cd9aab5997decda830
Yunoxa <agentreborn3@gmail.com>
2024-02-18 04:39:47 +0000

    oops

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://www.yunoxa.moe",
   base: "/",
   integrations: [
     tailwind(),
